45a770922cb49470554cab5995c96d72660db258
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-10-08 17:19:41 -0400

    trying to get operators to display

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
-import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+
+
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -20,6 +23,7 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Numbers /> 
+        <Operators /> 
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NumberButton = props => {
-  console.log("NumberButton props", props)
+  console.log("NumberButton props", props);
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
